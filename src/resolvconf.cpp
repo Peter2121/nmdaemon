6132c85f0e1a84c69ab030bf0774837dabf4dae6
Peter <peter@flytrace.com>
2024-01-18 18:55:08 +0100

    Implement RESOLVCONF_WRITE

--- src/resolvconf.cpp
+++ src/resolvconf.cpp
@@ -105,6 +105,73 @@ json ResolvConf::getConfig()
 
 bool ResolvConf::setConfig(json data)
 {
-    LOG_S(ERROR) << "setConfig is not (yet) implemented";
-    return false;
+/*
+{
+    "SCOPE" : "SYSTEM",
+    "CMD" : "RESOLVCONF_WRITE",
+    "DATA": {
+        "DOMAIN": "domain1.local",
+        "NAMESERVERS": [
+          "8.8.8.8",
+          "4.4.4.4"
+        ],
+        "SEARCH DOMAINS": [
+          "domain1.local",
+          "domain2.local"
+        ]
+    }
+}
+*/
+    std::string domain = "";
+    json jar_nameservers = json::array();
+    std::vector<std::string> nameservers;
+    json jar_search_domains = json::array();
+    std::vector<std::string> search_domains;
+    try
+    {
+        domain = data[JSON_PARAM_HOST_DOMAIN];
+        jar_nameservers = data[JSON_PARAM_NAMESERVERS];
+        jar_search_domains = data[JSON_PARAM_SEARCH_DOMAINS];
+    }
+    catch (std::exception& e) {
+        LOG_S(ERROR) << "ResolvConf::setConfig - cannot get resolv.conf parameters from json: " << e.what();
+        return false;
+    }
+    for(const auto &ns : jar_nameservers)
+    {
+        nameservers.push_back(ns);
+    }
+    for(const auto &sd : jar_search_domains)
+    {
+        search_domains.push_back(sd);
+    }
+    try
+    {
+        LOG_S(INFO) << "Writing to " << confFileName << std::endl;
+        std::ofstream resolconf_file(confFileName, std::ios::trunc);
+        resolconf_file << DOMAIN << " " << domain << std::endl;
+        if(!search_domains.empty())
+        {
+            resolconf_file << SEARCH;
+            for(const auto &sd : search_domains)
+            {
+                resolconf_file << " " << sd;
+            }
+            resolconf_file << std::endl;
+        }
+        if(!nameservers.empty())
+        {
+            for(const auto &ns : nameservers)
+            {
+                resolconf_file << NAMESERVER << " " << ns << std::endl;
+            }
+        }
+        resolconf_file << std::endl;
+        resolconf_file.close();
+    }
+    catch (std::exception& e) {
+        LOG_S(ERROR) << "ResolvConf::setConfig - cannot write to " << confFileName << " : " << e.what();
+        return false;
+    }
+    return true;
 }
