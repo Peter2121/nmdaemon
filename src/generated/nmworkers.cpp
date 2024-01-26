1775840c08b2e1158b3b504573724f12fec24afc
Peter <peter@flytrace.com>
2024-01-26 20:43:25 +0100

    Use version from env, remove generated dir from repo

--- src/generated/nmworkers.cpp
+++
@@ -1,6 +0,0 @@
-workers.push_back(new NmWorkerDummy());
-workers.push_back(new NmWorkerSys());
-workers.push_back(new NmWorkerIf());
-workers.push_back(new NmWorkerRoute());
-workers.push_back(new NmWorkerWpa());
-workers.push_back(new NmWorkerIeee80211());
--- src/generated/nmworkers.h
+++
@@ -1,6 +0,0 @@
-#include "../nmworkerdummy.h"
-#include "../nmworkersys.h"
-#include "../nmworkerif.h"
-#include "../nmworkerroute.h"
-#include "../nmworkerwpa.h"
-#include "../nmworkerieee80211.h"
